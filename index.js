e8f010419cfa30e19a2052bc76e453c2cb8dc085
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-14 17:54:29 +0200

    getelements

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import { catsData } from "./data.js";
 
 const emotionRadios = document.getElementById("emotion-radios");
 
-emotionRadios.addEventListener("change", (e) => {
-  console.log(e.target.id);
-});
+emotionRadios.addEventListener("change", highlightCheckedOption);
+function highlightCheckedOption(e) {
+  let radios = document.getElementsByClassName("radio");
+  for (let radio of radios) {
+    radio.classList.remove("highlight");
+  }
+  document.getElementById(e.target.id).parentElement.classList.add("highlight");
+}
 
 function getEmotionsArray(cats) {
   const emotionsArray = [];
