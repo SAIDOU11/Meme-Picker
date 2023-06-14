fd9eb1350aafe8fab1f944571e4dcfa7c5762e72
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-14 17:04:06 +0200

    classlist

---
+++ Aside includes/index.js
@@ -0,0 +1,26 @@
+const emojis = ["🦆", "🐯", "🐼"];
+console.log(emojis.includes("📦"));
+
+const addItemBtn = document.getElementById("add-item-btn");
+const itemInput = document.getElementById("item-input");
+const list = document.getElementById("list");
+
+const shoppingList = [];
+
+addItemBtn.addEventListener("click", () => {
+  if (shoppingList.includes(itemInput.value)) {
+    console.log("No Duplicate");
+  } else {
+    shoppingList.push(itemInput.value);
+    render();
+  }
+  itemInput.value = "";
+});
+
+function render() {
+  let html = "";
+  for (let item of shoppingList) {
+    html += `<li class="list-item">${item}</li>`;
+  }
+  list.innerHTML = html;
+}
